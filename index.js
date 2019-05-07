6962508c43c96a1d4e585103430e039199aca128
aaaaus <ausluft@gmail.com>
2019-05-07 18:41:54 -0400

    initial implementation of google oauth via passport js, nodemon dependency installed

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+// app.get('/', (req, res) => {
+//   res.send({ hi: 'there' });
+// });
+
+passport.use(
+  new GoogleStrategy( //has an internal identifier called 'google', used in authenticate function
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback' //URL after google authentication
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token', accessToken);
+      console.log('refresh token', refreshToken);
+      console.log('profile', profile);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'] //this is the information we are requesting from Google, see documentation
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000; //Heroku compatibility
 app.listen(PORT);
