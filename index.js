ab5a057a93a050c6cd3fc2f4c7987cf2daaded90
aaaaus <ausluft@gmail.com>
2019-05-08 15:24:49 -0400

    passport configuration, mongoose configuration, cookie-session install and configuration

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys'); //needed for mongoose.connect call
+require('./models/User'); //must precede passport, which needs this to exist first
 require('./services/passport'); //as we're not exporting anything from passport.js, does not need to be set to variable
 //const authRoutes = require('./routes/authRoutes'); see refactor below
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //authRoutes(app); see refactor below
 require('./routes/authRoutes')(app); //passing in app sets up oauth routes
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,19 @@
 const passport = require('passport');
+const mongoose = require('mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id); //this is the mongo id, not the oauth id (oauth agnostic)
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy( //has an internal identifier called 'google', used in passport.authenticate function
@@ -10,9 +23,18 @@ passport.use(
       callbackURL: '/auth/google/callback' //URL after google authentication
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile', profile);
+      //async operation, use promise
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //we already have an instance of this user
+          done(null, existingUser);
+        } else {
+          //we don't have this user, make a new record
+          new User({ googleId: profile.id })
+            .save()
+            .then(newUser => done(null, newUser)); //call save to persist to db
+        }
+      });
     }
   )
 );
