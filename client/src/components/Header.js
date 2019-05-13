fb059ca5182ce4f5c99c4beb0e2993ce5909cac3
aaaaus <ausluft@gmail.com>
2019-05-13 11:48:26 -0400

    completed auth routing on frontend, router linking for logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,47 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
+    console.log('PROPS ARE', this.props);
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">SurveyShark</a>
-          <ul className="right">
-            <li>
-              <a>Login with Google</a>
-            </li>
-          </ul>
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
+            className="left brand-logo"
+          >
+            SurveyShark
+          </Link>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
+export default connect(mapStateToProps)(Header);
