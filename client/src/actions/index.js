cc50f8c6bec51136e0935b211edaf9fc1785927d
aaaaus <ausluft@gmail.com>
2019-05-12 23:08:09 -0400

    fetchUser refactor, authReducer configuration

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,10 +2,16 @@ import axios from 'axios';
 
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => {
-  return function(dispatch) {
-    axios
-      .get('/api/current_user')
-      .then(res => dispatch({ type: FETCH_USER, payload: res }));
-  };
+// export const fetchUser = () => {
+//   return function(dispatch) {
+//     axios
+//       .get('/api/current_user')
+//       .then(res => dispatch({ type: FETCH_USER, payload: res }));
+//   };
+// };
+
+//REFACTOR: with arrow functions, if only expression is return statement, you can drop curly braces and return statement
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get('/api/current_user');
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
